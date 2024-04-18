3bd6124fef16788e0db47247235b25c6492344c8
S2Hyeon <tmdgus908@naver.com>
2024-04-18 16:52:36 +0900

    N과 M (4)

---
+++ BOJ/BOJ_15652.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// [S3] N과 M (4)
+public class BOJ_15652 {
+    static int N, M;
+    static int[] numbers;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        numbers = new int[M];
+        sb = new StringBuilder();
+
+        combination(0, 1);
+        System.out.println(sb);
+    }
+
+    private static void combination(int index, int cur) {
+        if(index == M) {
+            for(int i = 0; i < M; i++) {
+                sb.append(numbers[i]).append(" ");
+            }
+
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = cur; i <= N; i++) {
+            numbers[index] = i;
+            combination(index + 1, i);
+        }
+    }
+}
